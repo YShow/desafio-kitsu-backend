ba39f4f465295dc8b1397fc9390d68fdf97a3cdb
Yasser Bazzi <yassermbb@gmail.com>
2021-12-30 18:29:40 -0300

    inicio do projeto

---
+++ src/test/java/com/yshow/firedev/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yshow.firedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
