a9fe62ced70e45e3be03956b074fc7ff651dc7c6
Yasser Bazzi <yassermbb@gmail.com>
2021-12-31 02:40:39 -0300

    Swagger 3 adiconado e Otimizado a query para a kitsu

--- README.md
+++ README.md
@@ -1,59 +1,17 @@
 # Desafio Kitsu - Firedev IT
+Minha tentaiva em fazer o desafio Kitsu da Firedev
 
-Olá Dev! Tudo bem?
+## Tecnologias
+[Spring Framework](https://spring.io)
 
-Nós estamos sempre em busca de profissionais interessantes e interessados, com boa capacidade de aprendizado, adaptação e principalmente bom senso!
+[Java 17](https://jdk.java.net/17)
 
-Este teste tem como objetivo avaliar e desafiar você. Não é obrigatório realizá-lo completamente, queremos apenas reconhecer seu esforço e potencial para aprender, se adaptar e tomar decisões.
+[Eclipse IDE](https://www.eclipse.org)
 
-Vamos usar esse teste para validar a sua lógica, design da sua solução, código legível, estruturação do código, entre outros fatores.
+[Maven](https://maven.apache.org)
+##
 
-Vamos ao teste!
-
-## Desafio
-
-Realizar consultas na API [https://kitsu.docs.apiary.io](https://kitsu.docs.apiary.io)
-
-Construir os seguintes endpoints:
- - Retorno dos animes mais populares;
- - Retorno dos mangas mais populares;
- - Buscar anime ou manga com filtros (título, ano, etc);
- - Obter informações de um anime ou manga;
- - Obter informações de um episodio de um anime especifico;
-
-## Regras
-
- 1. Linguagens JAVA ou PHP.
- 2. Poderá ser feito utilizando qualquer framework.
- 3. O código deve ser orientado a objeto.
- 4. Deve ser versionado usando o git e disponibilizado no GitHub ou Bitbucket.
- 5. Descrever no README do projeto as especificações, tecnologias e demais informações do projeto.
-
-## Plus
-
- 1. Deploy no Heroku.
- 2. Criar estrutura no banco para salvar as buscas do usuário.
- 3. Criar um relatório de itens mais pesquisados com base na estrutura criada.
-
-## O que apreciamos?
-
- - Organização;
- - Simplicidade;
- - Objetividade;
- - Reúso de código;
- - Testes unitários e ou de integração;
- - Padronização de código;
- - Padrões de projeto;
- - Documentação de código e endpoints.
-
-## Quem buscamos?
-
-Queremos uma pessoa que goste do que faz, trabalhe em equipe e tenha vontade de inovar, buscando sempre atualização e soluções inovadoras.
-
-Se você se identificou, venha fazer parte do nosso time!
-
-## 
-
-Qualquer duvida pode entrar em contato com o recrutador.
-
-_Não precisa desenvolver uma interface gráfica, porem é um diferencial._
+## Instruções para compilar e rodar
+1. Certifique que JAVA_HOME aponte para a versao do java 17 ou mais
+2. Digite o comando `mvnw install`
+3. Execute o jar com o comando `java -jar firedev-1.0.0.jar`
--- src/main/java/com/yshow/firedev/anime/AnimeRestController.java
+++ src/main/java/com/yshow/firedev/anime/AnimeRestController.java
@@ -2,9 +2,11 @@ package com.yshow.firedev.anime;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.yshow.firedev.KitsuClient;
@@ -13,52 +15,62 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class AnimeRestController {
-    Logger logger = LoggerFactory.getLogger(AnimeRestController.class);
+    @Autowired
+    private WebClient kitsuWebClient;
+    
+    public AnimeRestController(WebClient kitsuWebClient) {
+	this.kitsuWebClient = kitsuWebClient;
+    }
+    
     @GetMapping("/anime/populares")
     Mono<Object> populares() {
-      return KitsuClient.createKitsuWebClient().get()
+      return kitsuWebClient.get()
 	      .uri("/trending/anime")
 	      .exchangeToMono(response -> response.bodyToMono(Object.class));
     }
     
     @GetMapping("/anime/filtrar")
     public Mono<Object> pesquisarPorFiltro(@RequestParam String filtro,@RequestParam String texto) {
- 	return KitsuClient.createKitsuWebClient().get()
+ 	return kitsuWebClient.get()
  		      .uri("anime?filter[{filtro}]={texto}",filtro,texto)
  		      .exchangeToMono(response -> response.bodyToMono(Object.class));
      }
      
      @GetMapping("/anime")
      public Mono<Object> pesquisarAnime(@RequestParam(name = "nome") String nomeAnime){	
- 	return KitsuClient.createKitsuWebClient().get()
+ 	return kitsuWebClient.get()
  		      .uri("anime?filter[text]={nomeAnime}",nomeAnime)
  		      .exchangeToMono(response -> response.bodyToMono(Object.class));
      }
      
      
      @GetMapping("/anime/episodio")
-     public Mono<JsonNode> pesquisarAnimeEpisodio(@RequestParam(name = "nome") String nomeAnime,@RequestParam(name = "ep") Integer ep){
-	 //retornamos o primeiro anime na pesquisa por nome
- 	final var resposta = KitsuClient.createKitsuWebClient().get()
-		      .uri("anime?filter[text]={nomeAnime}",nomeAnime)
-		      .retrieve()
-		      .bodyToMono(JsonNode.class);
- 		      // pegamos a lista JSON
-		     final var animes = resposta.block();
-		      if(animes != null) {
-			  /*
-			   o id é uma key para identificar o anime na api
-			   */
-			  final String id = filtrarId(animes);
-			  /*Em vez de usar uma logica propria sera usado a json api para ver o episodio
-			    https://kitsu.docs.apiary.io/#introduction/json:api/filtering-and-search.
-			    Na api de episodios todo json possui a key "number" onde é identificado o episodio em questao
-			  */
-			  return KitsuClient.createKitsuWebClient().get().uri("anime/{id}/episodes?filter[number]={ep}",id,ep).retrieve()
-			      .bodyToMono(JsonNode.class);
-			 
-		      }
- 	
+     public Mono<JsonNode> pesquisarAnimeEpisodio(@RequestParam(name = "nome") String nomeAnime,
+	     @RequestParam(name = "ep") Integer episodio) {
+	 /* retornamos o primeiro anime na pesquisa por nome
+	  * para nao termos que retornar o json inteiro usaremos a json api
+	  * para apenas nos mandar o id do anime
+	  */
+	 final var resposta = kitsuWebClient.get().uri("anime?fields[anime]=id&filter[text]={nomeAnime}", nomeAnime)
+		 .retrieve().bodyToMono(JsonNode.class);
+	 // pegamos a lista JSON
+	 final var animes = resposta.block();
+	 if (animes != null) {
+	     /*
+	      * o id é uma key para identificar o anime na api
+	      */
+	     final String id = filtrarId(animes);
+	     /*
+	      * Em vez de usar uma logica propria sera usado a json api para ver o episodio
+	      * https://kitsu.docs.apiary.io/#introduction/json:api/filtering-and-search. Na
+	      * api de episodios todo json possui a key "number" onde é identificado o
+	      * episodio em questao
+	      */
+	     return kitsuWebClient.get().uri("anime/{id}/episodes?filter[number]={ep}", id, episodio)
+		     .retrieve().bodyToMono(JsonNode.class);
+
+	 }
+
 	 return resposta;
      }
      
